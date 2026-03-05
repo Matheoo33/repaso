9119d2d7cabb77855a55f96bf017f8c4bf317bcb
Matheoo <matheoplovanich09@gmail.com>
2026-03-05 12:54:08 -0300

    Update 03.py

--- 03.py
+++ 03.py
@@ -1,1 +1,10 @@
+while contador <= 5:
+    print(f"Estamos en el paso número: {contador}")
+    contador += 1
+    print("¡Ciclo terminado!")
+    
+          
+          
+    for i in range(5):
+    print(i)
 
